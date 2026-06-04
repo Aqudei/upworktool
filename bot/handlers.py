ec54089ea03de79391720145e350b189735bc004
aqudei <fdeu.aalmine@gmail.com>
2026-06-04 21:43:30 +0800

    up

--- bot/handlers.py
+++ bot/handlers.py
@@ -46,7 +46,7 @@ async def fetch_jobs(context: ContextTypes.DEFAULT_TYPE):
             "query": query,
             "variables": {
                 "filter": {
-                    "pagination_eq": {"after": "0", "first":10}
+                    "pagination_eq": {"after": "0", "first":50}
                 }
             }
         } 
